ec10f6b725c34a4e9d982b1733e89d5ecdb869d8
ntkntk123 <nguyentrongkhai13022005@gmail.com>
2024-11-16 23:06:00 +0700

    khai

---
+++ commonsz/env.php
@@ -0,0 +1,11 @@
+<?php 
+
+define('BASE_URL'       , 'C:\laragon\www\duan1\duan1\Nhom10z');
+
+define('DB_HOST'    , 'localhost');
+define('DB_PORT'    , 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME'    , 'duan');  // Tên database
+
+define('PATH_ROOT'    , __DIR__ . '/../');
